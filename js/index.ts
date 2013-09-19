7c0e880444bf19c1f95707c340b50cb0bfd4599e
bertyhell <verhelstbert@gmail.com>
2013-09-19 16:58:37 -0700

    created program v0.1

--- js/index.ts
+++ js/index.ts
@@ -1,21 +1,22 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="iscroll.d.ts" />
 /// <reference path="../cordova.d.ts" />
-var currentDir;
-var driveViewScroller;
-var currentFolderItems;
+
+var currentDir: DirectoryEntry;
+var driveViewScroller: iScroll;
+var currentFolderItems: Array<any>;
 
 document.addEventListener('deviceready', function () {
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, failRoot);
 
     driveViewScroller = new iScroll('driveViewScroller', { hScroll: false, hScrollbar: false, vScrollbar: true });
 
-    $("#btnPreview").click(function () {
+    $("#btnPreview").click(function() {
         //preview of renamed files
         var files = $("li");
-        for (var i = 1; i < files.length; i++) {
-            var folderName = $(files[i]).attr("name");
-            var regex = new RegExp($("#regexSearch").val(), "g");
+        for (var i = 1; i < files.length; i++) { //skip top folder
+            var folderName: string = $(files[i]).attr("name");
+            var regex = new RegExp($("#regexSearch").val(),"g");
 
             if (folderName.match(regex)) {
                 var previewName = folderName.replace(regex, $("#regexReplace").val());
@@ -27,9 +28,9 @@ document.addEventListener('deviceready', function () {
     $("#btnRename").click(function () {
         //rename files according to preview
         var files = $("li");
-        for (var i = 1; i < files.length; i++) {
-            var currentName = $(files[i]).attr("name");
-            var newName = $(files[i]).find("span + span").html();
+        for (var i = 1; i < files.length; i++) { //skip top folder
+            var currentName: string = $(files[i]).attr("name");
+            var newName: string = $(files[i]).find("span + span").html();
 
             if (currentFolderItems[i - 1].name == currentName) {
                 window.console.log("renaming file: " + currentName + " => " + newName);
@@ -43,9 +44,8 @@ document.addEventListener('deviceready', function () {
     });
 });
 
-document.addEventListener('touchmove', function (e) {
-    e.preventDefault();
-}, false);
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
 
 function onFileSystemSuccess(fileSystem) {
     window.console.log("onFileSystemSuccess");
@@ -59,24 +59,24 @@ function updateFilesList(entries) {
     driveView.html("");
     currentFolderItems = entries;
 
-    driveView.append("<li class='folder'>..</li>");
-    for (var i = 0; i < entries.length; i++) {
+    driveView.append("<li class='folder'>..</li>")
+    for (var i: number = 0; i < entries.length; i++) {
         if (entries[i].isFile) {
-            driveView.append("<li class='file' name='" + entries[i].name + "'><span>" + entries[i].name + "</span><span>" + entries[i].name + "</span></li>");
+            driveView.append("<li class='file' name='" + entries[i].name+"'><span>" + entries[i].name + "</span><span>" + entries[i].name + "</span></li>")
         } else {
-            driveView.append("<li class='folder' name='" + entries[i].name + "'><span>" + entries[i].name + "</span><span>" + entries[i].name + "</span></li>");
+            driveView.append("<li class='folder' name='" + entries[i].name +"'><span>" + entries[i].name + "</span><span>" + entries[i].name + "</span></li>")
         }
     }
 
     $("li[class='folder']").click(function (element) {
+        //on click of a folder
         if ($(element.currentTarget).html() == "..") {
             //move on up
             //alert("move folder up");
             currentDir.getParent(onGetDirectorySuccess, onGetDirectoryFail);
         } else {
             if ($(element.currentTarget).hasClass("folder")) {
-                var subFolder = $(element.currentTarget).find("span:first-child").html();
-
+                var subFolder: string = $(element.currentTarget).find("span:first-child").html();
                 //alert("move to subfolder: " + subFolder);
                 currentDir.getDirectory(subFolder, { create: false, exclusive: false }, onGetDirectorySuccess, onGetDirectoryFail);
             }
@@ -92,7 +92,7 @@ function updateScroller() {
     //driveViewScroller.scrollTo(0, 0, 200);
 }
 
-function onGetDirectorySuccess(directory) {
+function onGetDirectorySuccess(directory: DirectoryEntry) {
     window.console.log("onGetDirectorySuccess");
     currentDir = directory;
     currentDir.createReader().readEntries(updateFilesList, onGetSubDirectoriesFail);
